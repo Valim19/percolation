da51e544cae9da5e2e86783e6e29207fbeb39351
Anna Valim <annacarolinavalim@gmail.com>
2024-10-01 18:16:30 -0300

    modificações no arquivo Percolation.java

--- Percolation.java
+++ Percolation.java
@@ -2,23 +2,58 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-// Program to estimate the value of the percolation threshold via Monte Carlo simulation
-
-// Corner cases.  By convention, the row and column indices are integers between 1 and n,
-// where (1, 1) is the upper-left site: Throw an IllegalArgumentException if any argument to open(),
-// isOpen(), or isFull() is outside its prescribed range. Throw an IllegalArgumentException in the constructor if n ≤ 0
-
-// Running Time must be proportional to n²
-
 public class Percolation {
-    // creates n-by-n grid, with all sites initially blocked
-    public Percolation(){
+    private static final int TOP = 0;
+    private final int bottom;
+    private int size;
+    private int openSitesCounter;
+    private boolean[][] opened;
+    private final WeightedQuickUnionUF quickUnionUF;
 
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n){
+        if (n <= 0){
+            throw new IllegalArgumentException();
+        }
+
+        size             = n;
+        bottom           = (size * size) + 1;
+        opened           = new boolean[size][size]; // Creates a matrice with all the sites setted to false
+        quickUnionUF     = new WeightedQuickUnionUF((size * size) + 2);
+        openSitesCounter = 0;
     }
 
-    // opens the site (row, col) if it is not open already
     public void open(int row, int col){
+        if (row <= 0 || row > size || col <= 0 || col > size) {
+            throw new IllegalArgumentException();
+        }
 
+        opened[row - 1][col - 1] = true;
+        openSitesCounter++;
+
+        if(row == 1){
+            quickUnionUF.union(getQuickFindIndex(row, col), TOP);
+        }
+
+        if(row == size){
+            quickUnionUF.union(getQuickFindIndex(row, col), bottom);
+        }
+
+        if(row > 1 && isOpen(row - 1, col)){
+            quickUnionUF.union(getQuickFindIndex(row, col), getQuickFindIndex(row - 1, col));
+        }
+
+        if(row < size && isOpen(row + 1, col)){
+            quickUnionUF.union(getQuickFindIndex(row, col), getQuickFindIndex(row + 1, col));
+        }
+
+        if(col > 1 && isOpen(row, col - 1)){
+            quickUnionUF.union(getQuickFindIndex(row, col), getQuickFindIndex(row, col - 1));
+        }
+
+        if(col < size && isOpen(row, col + 1)){
+            quickUnionUF.union(getQuickFindIndex(row, col), getQuickFindIndex(row, col + 1));
+        }
     }
 
     // is the site (row, col) open?
@@ -46,3 +81,4 @@ public class Percolation {
 
     }
 }
+
